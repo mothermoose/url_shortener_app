807a8db0f8bab9582aac9a30763a9df01edc0298
mothermoose <melanie.ilseman@gmail.com>
2017-05-31 10:02:20 -0500

    added links controller and model, stopped at error undefined method 'get'

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,6 @@ Rails.application.routes.draw do
    end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
+end 
+
+get '/links' => 'links#index'
